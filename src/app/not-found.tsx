a5f7f16e511b1d6db5d341acbda5157bbc89738b
aibiansari <aibiansari1@gmail.com>
2024-10-03 09:46:33 +0500

    not-found fixed

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,27 +3,15 @@ import Link from "next/link";
 // Page shown when the user tries to access a page that doesn't exist
 export default function NotFound() {
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <div className=" flex flex-col items-center justify-center border-2 border-neutral-400 dark:border-neutral-950 p-12 rounded-xl shadow-lg shadow-black/50">
-        <div className="mb-4">
-          <picture>
-            <img
-              src="./logo.svg"
-              alt="Linkrary Logo"
-              loading="lazy"
-              className="w-24 h-24 invert dark:invert-0"
-            />
-          </picture>
-        </div>
-        <h1 className="text-4xl font-medium mb-6">Lost in the Linkrary!</h1>
-
-        <Link
-          href="/"
-          className="px-6 py-3 bg-neutral-300 hover:bg-neutral-400 rounded-full dark:bg-neutral-800 dark:hover:bg-neutral-700 transition-all"
-        >
-          Go Back Home
-        </Link>
-      </div>
+    <div className="min-h-screen font-mono flex flex-col items-center justify-center">
+      <h1 className="text-8xl mb-4">404</h1>
+      <h1 className="text-4xl font-medium mb-6">Lost in the Linkrary!</h1>
+      <Link
+        href="/"
+        className="px-6 py-3 text-white rounded-full bg-neutral-600 hover:bg-neutral-700 transition-colors duration-300"
+      >
+        Go Back Home
+      </Link>
     </div>
   );
 }
